c06824b2ca5c59a5e700959b1bc57dc7180775b4
Artem Ryzhenkov <RyzhenkovAA@yandex.ru>
2023-11-30 00:34:40 +0300

    v8.3.1

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -8,17 +8,35 @@ from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
 from django.urls import reverse_lazy
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 #from django.core.paginator import Paginator
 
 
-class BirthdayCreateView(CreateView):
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
     model = Birthday
     form_class = BirthdayForm
 
+    def form_valid(self, form):
+        # Присвоить полю author объект пользователя из запроса.
+        form.instance.author = self.request.user
+        # Продолжить валидацию, описанную в форме.
+        return super().form_valid(form)  
 
-class BirthdayUpdateView(UpdateView):
+
+class BirthdayUpdateView(LoginRequiredMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
+
+    def dispatch(self, request, *args, **kwargs):
+        # Получаем объект по первичному ключу и автору или вызываем 404 ошибку.
+        get_object_or_404(Birthday, pk=kwargs['pk'], author=request.user)
+        # Если объект был найден, то вызываем родительский метод, 
+        # чтобы работа CBV продолжилась.
+        return super().dispatch(request, *args, **kwargs) 
 
 #def birthday(request, pk=None):
 #    if pk is not None:
@@ -52,7 +70,7 @@ class BirthdayListView(ListView):
 #    return render(request, 'birthday/birthday_list.html', context)
 
 
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(LoginRequiredMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
@@ -74,3 +92,7 @@ class BirthdayDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['birthday_countdown'] = calculate_birthday_countdown(self.object.birthday)
         return context
+
+@login_required
+def simple_view(request):
+    return HttpResponse('Страница для залогиненных пользователей!')
